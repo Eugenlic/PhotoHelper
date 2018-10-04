5d65cea2786dac8e176976a8aa9f549cd88065eb
Eugen Licak <eugen.licak@kodep.ru>
2018-10-04 14:32:27 +0300

    add_active_admin

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,15 +1,54 @@
 ActiveAdmin.register Order do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :status, :price, :image
+  actions :all, except: :edit
+  index do
+    column :user
+    column :description
+    column :image do |order|
+      image_tag(order.image_url(:thumb))
+    end
+    column :status
+    column :price
+    actions
+  end
 
+  show do
+    attributes_table do
+      row :id
+      row :user
+      row :description
+      row :image do
+        link_to image_tag(order.image_url(:thumb)), order.image_url(:large)
+      end
+      row :status
+      row :price
+    end
+    active_admin_comments
+  end
+
+  action_item :to_value, only: :show do
+    link_to t('.to_value'), to_value_admin_order_path if order.considered?
+  end
+
+  action_item :attach_photo, only: :show do
+    link_to t('.attach_photo'), attach_photo_admin_order_path if order.in_work?
+  end
+
+  member_action :to_value, title: I18n.t('active_admin.to_value') do
+    @order = Order.find(params[:id])
+    render :assessments
+  end
+
+  member_action :attach_photo, title: I18n.t('active_admin.attach_photo') do
+    @order = Order.find(params[:id])
+    render :attach_photo
+  end
+
+  controller do
+    def update
+      resource.valuated! if resource.considered?
+      resource.ready! if resource.in_work?
+      super
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_083723) do
+ActiveRecord::Schema.define(version: 2018_10_02_092638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_083723) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "status", default: 0, null: false
+    t.decimal "price"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
